3d804c063f78f714c24b2d524aa86eb08d2b9c76
Muntaz <mkhb654@gmail.com>
2015-06-02 15:20:09 -0400

    Hello World

---
+++ RunJavaScript.js
@@ -0,0 +1,10 @@
+var myData;
+ var myString = "Some Text";
+
+var arr =["one", "two"]
+var first=arr[0];
+
+var id = "StringID";
+console.log("l®®et®® see how well this work with what needs to be done");
+
+//how to add angular to thi
