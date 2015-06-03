698c49f5aa15db6bc9cf4bfc9d73ec58807a3117
Muntaz <mkhb654@gmail.com>
2015-06-03 10:28:17 -0400

    Working with if  else, anonymous Functions ,

---
+++ LinkedList.js
@@ -0,0 +1,8 @@
+/**
+ * Created by mkaleem on 6/2/15.
+ */
+//working with linkedlist using javascript
+
+
+function LinkedList() {}
+LinkedList.prototype = { Length:0, first:null}
--- RunJavaScript.js
+++ RunJavaScript.js
@@ -7,4 +7,37 @@ var first=arr[0];
 var id = "StringID";
 console.log("l®®et®® see how well this work with what needs to be done");
 
-//how to add angular to thi
+//how to add angular to the projects the project....// to work withi
+
+if (x==5) {
+    do_something();
+}else {
+    do_something_else();
+
+
+
+}
+//while loop
+
+var i= 1;
+while (i<5){
+    console.log("iteration"  +i + "<br>")
+
+}
+
+ for (var x=1; x<3;x++){
+     console.log (x
+
+
+//using Anonymous Function in JavaScript  ---
+
+function doCalc(num1, num2, calcFunction){
+  return calcFunction(num1, num2)
+
+    var myString = "Teach yourself Jquery& Javascript in 24"
+
+   // =======================
+
+
+}
+ }
